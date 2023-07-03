02cde0ea0e59359879e4c4085f51f2520458ce50
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-07-03 16:17:52 +0300

    completed assignment

--- index.js
+++ index.js
@@ -6,3 +6,35 @@ const welcomeMessage = `Welcome to Flatbook,  + ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
 const shortGreeting = `Welcome, ${currentUser}`;
+
+function distanceFromHqInBlocks(beginning, end) {
+    return end - beginning;
+}
+function distanceFromHqInFeet(distanceInFeet) {
+    distanceFromHqInBlocks(x);
+    return x * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return (destination-start)* 264
+    
+}
+
+function calculatesFarePrice(start, destination) {
+    if(distanceTravelledInFeet <= 400) {
+        return 'Free';
+        
+    }
+    else if(distanceTravelledInFeet > 400 && distanceTravelledInFeet <= 2000) {
+        return 'the price is 2 cents';
+    }
+    else if(distanceTravelledInFeet > 2000 && distanceTravelledInFeet < 2500) {
+        return 'the scuber charges a flat rate';
+        
+        
+    }
+    else if(distanceTravelledInFeet > 2500) {
+        return "cannot travel that far";
+
+    
+}
